5fe26a2a5d6e12e52bf3f9486245113417c96d82
EduardoFabeniJohannsen <eduardofabenijohannsen@email.com>
2026-03-19 08:03:17 -0300

    Finalizado exercicios, sem testar

--- Banco de Dados-TER/Aula_03_17-03/codigo.sql
+++ Banco de Dados-TER/Aula_03_17-03/codigo.sql
@@ -1,9 +1,17 @@
 -- Creates basicos para começar a atividade
 CREATE DATABASE aula_Emerson;
+
 CREATE TABLE Aluno(
 	id SERIAL PRIMARY KEY,
 	nome TEXT NOT NULL	
 );
+
+CREATE TABLE LOG_Exclusoes(
+	id SERIAL PRIMARY KEY,
+	id_aluno INT FOREN KEY REFERENCES Aluno(id),
+	nome TEXT NOT NULL,
+	data_exclusao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
 
 -- COMEÇO -> Comando para inserir/apagar/ver os dados e testar
 INSERT INTO Aluno
@@ -42,6 +50,7 @@ ON Aluno
 FOR EACH ROW
 EXECUTE FUNCTION Validar_Nome_Com_Sobrenome();
 
+
 -- Exercicio 2
 CREATE OR REPLACE FUNCTION Converter_nome_para_maiusculo()
 RETURNS TRIGGER AS $$
@@ -52,8 +61,42 @@ BEGIN
 		RETURN NEW;
 	END IF
 END;
-
-
-
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER Garantir_Nome_Maiusculo()
+BEFORE INSERT OR UPDATE
+ON Aluno
+FOR EACH ROW
+EXECUTE FUNCTION Converter_nome_para_maiusculo();
+
+-- Exercicio 3
+CREATE OR REPLACE FUNCTION Registrar_Exclusao_Aluno()
+RETURNS TRIGGER AS $$
+BEGIN
+	INSERT INTO LOG_Exclusoes(id_aluno, nome)
+	VALUES(OLD.id, OLD.nome);
+	
+	RAISE NOTICE 'Aluno "%" excluído e registrado no log', OLD.nome;
+	RETURN OLD;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Chamar_Registrar_Exclusao_Aluno()
+BEFORE DELETE
+ON Aluno
+FOR EACH ROW
+EXECUTE FUNCTION Registrar_Exclusao_Aluno();
+
+-- Exercicio 4
+CREATE OR REPLACE FUNCTION Avisar_Mudanva_Nome()
+RETURNS TRIGGER AS $$
+BEGIN
+	RAISE NOTICE 'Aluno "%" alterado nome para %', OLD.nome,NEW.nome;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Chamar_Avisar_Mudanva_Nome()
+BEFORE UPDATE
+ON Aluno
+FOR EACH ROW
+EXECUTE FUNCTION Chamar_Avisar_Mudanva_Nome();
