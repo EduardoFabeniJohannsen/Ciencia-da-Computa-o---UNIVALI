9f1f529706cb7d299ed06fba7a203ecfe83d193a
Eduardo Fabeni Johannsen <eduardofabenijohannsen@gmail.com>
2026-04-07 20:39:31 -0300

    Feito o exercicio 1

--- Banco_de_Dados-TER/Exercicios_M1/Exercicios.sql
+++ Banco_de_Dados-TER/Exercicios_M1/Exercicios.sql
@@ -1,2 +1,37 @@
-		preco_antigo NUMERIC(10,2),
-		preco_anovo NUMERIC(10,2),
+-- Exercicio 1
+CREATE OR REPLACE FUNCTION Auditar_Alteracao_Preco()
+RETURNS TRIGGER AS $$
+BEGIN
+
+    -- Só registra se o preço realmente mudou
+    IF OLD.preco <> NEW.preco THEN
+
+        INSERT INTO log_auditoria (
+            tabela_afetada,
+            id_registro_afetado,
+            operacao,
+            preco_antigo,
+            preco_novo,
+            data_operacao
+        )
+        VALUES (
+            'livros',
+            OLD.id_livro,
+            'UPDATE',
+            OLD.preco,
+            NEW.preco,
+            CURRENT_TIMESTAMP
+        );
+
+    END IF;
+
+    RETURN NEW;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Trigger_Auditar_Preco
+AFTER UPDATE
+ON livros
+FOR EACH ROW
+EXECUTE FUNCTION Auditar_Alteracao_Preco();
